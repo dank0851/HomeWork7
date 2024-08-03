1ad2a1a6f89a1ba575a9996adabe4dd4d46327b8
dank0851 <83114024+dank0851@users.noreply.github.com>
2024-08-04 01:31:24 +0300

    Add files via upload

---
+++ src/main/java/com/github/dank0851/ColoredFigure.java
@@ -0,0 +1,16 @@
+package com.github.dank0851;
+
+public abstract class ColoredFigure {
+    public String fillColor;
+    public String borderColor;
+
+    public ColoredFigure(String fillColor, String borderColor) {
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+
+    public String getColors() {
+        return " Цвет фона:" + fillColor +
+                "  Цвет границ:" + borderColor;
+    }
+}
---
+++ src/main/java/com/github/dank0851/Triangle.java
@@ -0,0 +1,31 @@
+package com.github.dank0851;
+
+public class Triangle extends ColoredFigure implements GeometricFigure {
+    public double a, b;
+
+    public Triangle(double a, double b, String fillColor, String borderColor) {
+        super(fillColor, borderColor);
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public double perimeter() {
+        double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        return this.a + this.b + c;
+    }
+
+    @Override
+    public double area() {
+        return this.a * this.b / 2.0;
+    }
+
+    @Override
+    public String toString() {
+        return "Треугольник [" +
+                "Катет a:" + a +
+                "  Катет b:" + b +
+                ' ' + this.getColors() +
+                ']';
+    }
+}
